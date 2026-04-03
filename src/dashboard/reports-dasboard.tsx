c22a198f6c9448f8ba4244bb66cccd3bfeb9a0a3
maikofelix47 <maikofelix89@gmail.com>
2026-04-03 11:36:57 +0300

    (feat) Add reports root component and dashboard link

---
+++ src/dashboard/reports-dasboard.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+interface ReportsDashboardProps {}
+const ReportsDashboard: React.FC<ReportsDashboardProps> = () => {
+  return <>AMPATH Reports and Registers</>;
+};
+
+export default ReportsDashboard;
