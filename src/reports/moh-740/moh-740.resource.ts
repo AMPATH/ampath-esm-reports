8bbe32b8dda1ba0e30c8bd66aca838babe7d90bc
maikofelix47 <maikofelix89@gmail.com>
2026-04-20 16:16:58 +0300

    (feat) Add MOH-740 Report

---
+++ src/reports/moh-740/moh-740.resource.ts
@@ -0,0 +1,14 @@
+import { getEtlBaseUrl } from '../../utils/get-base-url';
+import { type Moh740Resp, type Moh740Dto, type Moh740Data } from './types';
+
+export async function fetchMoh740Report(params: Moh740Dto): Promise<Moh740Data> {
+  const etlBaseUrl = await getEtlBaseUrl();
+  const moh740Url = `${etlBaseUrl}/moh-740-report?endDate=${params.endDate}&locationUuids=${params.locationUuid}`;
+  const resp = await fetch(moh740Url);
+  const data: Moh740Resp = await resp.json();
+  if (data && data.result) {
+    return data.result[0];
+  } else {
+    return null;
+  }
+}
