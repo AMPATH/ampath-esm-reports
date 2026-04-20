45db1f95470ce3d6c84ad786891555bf5a0fbc11
maikofelix47 <maikofelix89@gmail.com>
2026-04-20 17:12:47 +0300

    (fix) switch moh-740 from fetch to openmrsfetch

--- src/reports/moh-740/moh-740.resource.ts
+++ src/reports/moh-740/moh-740.resource.ts
@@ -1,10 +1,11 @@
+import { openmrsFetch } from '@openmrs/esm-framework';
 import { getEtlBaseUrl } from '../../utils/get-base-url';
 import { type Moh740Resp, type Moh740Dto, type Moh740Data } from './types';
 
 export async function fetchMoh740Report(params: Moh740Dto): Promise<Moh740Data> {
   const etlBaseUrl = await getEtlBaseUrl();
   const moh740Url = `${etlBaseUrl}/moh-740-report?endDate=${params.endDate}&locationUuids=${params.locationUuid}`;
-  const resp = await fetch(moh740Url);
+  const resp = await openmrsFetch(moh740Url);
   const data: Moh740Resp = await resp.json();
   if (data && data.result) {
     return data.result[0];
